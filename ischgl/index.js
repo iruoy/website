b20cc1379d17831cf50ecb6a28302eaa9f0b8147
Youri Wijnands <hey@iruoy.com>
2026-01-07 23:26:18 +0100

    ischgl meteoblue

--- ischgl/index.js
+++ ischgl/index.js
@@ -4,6 +4,9 @@ const items = document.getElementsByTagName('details');
 for (const item of items) {
   const date = new Date(item.children[0].textContent+'+01:00'); // only works for non dst dates
   const diff = date - new Date(); // only works if browser is in UTC+1 timezone
+  if (isNaN(diff)) {
+    continue;
+  }
   const days = Math.round(diff / 86400000); // 1 day in ms (24 * 60 * 60 * 1000)
 
   item.children[0].innerHTML += ' <small><em>(' + formatter.format(days, 'day') + ')</em></small>';
